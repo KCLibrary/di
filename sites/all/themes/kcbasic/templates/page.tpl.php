c54eedd8d2fabc1412ea331988521876eacf8d92
Jeff Petersen <jeff@juniperstudio.com>
2014-09-24 09:36:04 -0500

    update profile.

--- sites/all/themes/kcbasic/templates/page.tpl.php
+++ sites/all/themes/kcbasic/templates/page.tpl.php
@@ -1,7 +1,6 @@
-<div id="page" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <!-- ______________________ HEADER _______________________ -->
-
+<div id="header-full">
   <header id="header">
     <div class="container">
       <?php if ($logo): ?>
@@ -39,7 +38,11 @@
       <?php endif; ?>
     </div>
   </header> <!-- /header -->
+</div> <!-- head -->
 
+  <!-- ______________________ NAVAGATION _______________________ -->
+
+<div id="navigation-full">
   <?php if ($main_menu || $secondary_menu): ?>
     <nav id="navigation" class="menu <?php if (!empty($main_menu)) {print "with-primary";}
       if (!empty($secondary_menu)) {print " with-secondary";} ?>">
@@ -48,67 +51,71 @@
       </div>
     </nav> <!-- /navigation -->
   <?php endif; ?>
+<div>
 
-  <!-- ______________________ MAIN _______________________ -->
+  <!-- ______________________ PAGE _______________________ -->
 
-  <div id="main">
-    <div class="container">
-      <section id="content">
+<div id="page-full">
+  <div id="page" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+      <div class="container">
+        <section id="content">
+    
+            <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
+              <div id="content-header">
   
-          <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
-            <div id="content-header">
-
-              <?php print $breadcrumb; ?>
-
-              <?php if ($page['highlighted']): ?>
-                <div id="highlighted"><?php print render($page['highlighted']) ?></div>
-              <?php endif; ?>
-
-              <?php print render($title_prefix); ?>
-
-              <?php if ($title): ?>
-                <h1 class="title"><?php print $title; ?></h1>
-              <?php endif; ?>
-
-              <?php print render($title_suffix); ?>
-              <?php print $messages; ?>
-              <?php print render($page['help']); ?>
-
-              <?php if ($tabs): ?>
-                <div class="tabs"><?php print render($tabs); ?></div>
-              <?php endif; ?>
-
-              <?php if ($action_links): ?>
-                <ul class="action-links"><?php print render($action_links); ?></ul>
-              <?php endif; ?>
-
-            </div> <!-- /#content-header -->
-          <?php endif; ?>
-
-          <div id="content-area">
-            <?php print render($page['content']) ?>
-          </div>
-
-          <?php print $feed_icons; ?>
-
-      </section> <!-- /content-inner /content -->
-
-      <?php if ($page['sidebar_first']): ?>
-        <aside id="sidebar-first" class="column sidebar first">
-          <?php print render($page['sidebar_first']); ?>
-        </aside>
-      <?php endif; ?> <!-- /sidebar-first -->
-
-      <?php if ($page['sidebar_second']): ?>
-        <aside id="sidebar-second" class="column sidebar second">
-          <?php print render($page['sidebar_second']); ?>
-        </aside>
-      <?php endif; ?> <!-- /sidebar-second -->
-    </div>
-  </div> <!-- /main -->
+                <?php print $breadcrumb; ?>
+  
+                <?php if ($page['highlighted']): ?>
+                  <div id="highlighted"><?php print render($page['highlighted']) ?></div>
+                <?php endif; ?>
+  
+                <?php print render($title_prefix); ?>
+  
+                <?php if ($title): ?>
+                  <h1 class="title"><?php print $title; ?></h1>
+                <?php endif; ?>
+  
+                <?php print render($title_suffix); ?>
+                <?php print $messages; ?>
+                <?php print render($page['help']); ?>
+  
+                <?php if ($tabs): ?>
+                  <div class="tabs"><?php print render($tabs); ?></div>
+                <?php endif; ?>
+  
+                <?php if ($action_links): ?>
+                  <ul class="action-links"><?php print render($action_links); ?></ul>
+                <?php endif; ?>
+  
+              </div> <!-- /#content-header -->
+            <?php endif; ?>
+  
+            <div id="content-area">
+              <?php print render($page['content']) ?>
+            </div>
+  
+            <?php print $feed_icons; ?>
+  
+        </section> <!-- /content-inner /content -->
+  
+        <?php if ($page['sidebar_first']): ?>
+          <aside id="sidebar-first" class="column sidebar first">
+            <?php print render($page['sidebar_first']); ?>
+          </aside>
+        <?php endif; ?> <!-- /sidebar-first -->
+  
+        <?php if ($page['sidebar_second']): ?>
+          <aside id="sidebar-second" class="column sidebar second">
+            <?php print render($page['sidebar_second']); ?>
+          </aside>
+        <?php endif; ?> <!-- /sidebar-second -->
+      </div>
+    </div> <!-- /main -->
+  </div> <!-- /page -->
+</div>  
 
   <!-- ______________________ FOOTER _______________________ -->
-
+<div id="footer-full">
   <?php if ($page['footer']): ?>
     <footer id="footer">
       <div class="container">
@@ -116,5 +123,4 @@
       </div>
     </footer> <!-- /footer -->
   <?php endif; ?>
-
-</div> <!-- /page -->
+</div>
