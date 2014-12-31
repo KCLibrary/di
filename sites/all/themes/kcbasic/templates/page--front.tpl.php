af809eec0939ff440a0a56114b7e13adb88737a4
Jeff Petersen <jeff@juniperstudio.com>
2014-12-31 14:30:34 -0600

    templates

--- sites/all/themes/kcbasic/templates/page--front.tpl.php
+++ sites/all/themes/kcbasic/templates/page--front.tpl.php
@@ -59,42 +59,10 @@
 <div id="page-full">
   <div id="page" class="<?php print $classes; ?>"<?php print $attributes; ?>>
       <div class="container">
-        <section id="content">
-    
-            <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
-              <div id="content-header">
-  
-                <?php print $breadcrumb; ?>
-  
-                <?php if ($page['highlighted']): ?>
-                  <div id="highlighted"><?php print render($page['highlighted']) ?></div>
-                <?php endif; ?>
-  
-                <?php print render($title_prefix); ?>
-  
-                <?php if ($title): ?>
-                  <h1 class="title"><?php print $title; ?></h1>
-                <?php endif; ?>
-  
-                <?php print render($title_suffix); ?>
-                <?php print $messages; ?>
-                <?php print render($page['help']); ?>
-  
-                <?php if ($tabs): ?>
-                  <div class="tabs"><?php print render($tabs); ?></div>
-                <?php endif; ?>
-  
-                <?php if ($action_links): ?>
-                  <ul class="action-links"><?php print render($action_links); ?></ul>
-                <?php endif; ?>
-  
-              </div> <!-- /#content-header -->
-            <?php endif; ?>
-  
+        <section id="content"> 
             <div id="content-area">
-              <?php print render($page['content']) ?>
-            </div>
-    
+              <?php print render($page['front_content']) ?>
+            </div>  
         </section> <!-- /content-inner /content -->
   
         <?php if ($page['sidebar_first']): ?>
